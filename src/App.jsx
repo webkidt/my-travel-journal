2549679a132a836a7ecaf26612061460ab0c3790
David Pam <niratpam@gmail.com>
2022-10-13 23:48:21 +0100

    style card. add favico

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import Header from './components/Header';
+import Card from './components/Card';
 import './App.css'
 
 function App() {
   return (
     <div>
       <Header />
+      <section className='cards-list'>
+        <Card />
+      </section>
     </div>
   )
 }
